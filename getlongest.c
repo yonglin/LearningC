250883ac61ca353ebd834ab11c3a01f9827f9bcf
Yonglin <zhuoylin@gmail.com>
2013-01-19 20:24:33 +0800

    fuck

--- getlongest.c
+++ getlongest.c
@@ -1,46 +1,52 @@
 #include<stdio.h>
 
 #define MAXLINE 1000
-int getLine(char line[], int maxline);
-void copy(char to[], char from[]);
+
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int getline(void);
+void copy(void);
 
 main()
-{
 	int len;
-	int max;
-	char line[MAXLINE];
-	char longest[MAXLINE];
+	extern int max;
+	extern char longest[];
 
 	max = 0;
-	while ((len = getLine(line, MAXLINE)) > 0)
-			if (len > max){
-				max = len;
-				copy(longest, line);
-			}
+	while ((len = getline()) > 0)
+		if (len > max){
+			max = len;
+			copy();
+		}
 	if (max > 0)
 		printf("%s", longest);
 	return 0;
 }
 
-int getLine(char s[], int lim)
+int getline(void)
 {
 	int c, i;
+	extern char line[];
 
-	for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
-		s[i] = c;
-	if (c == '\n'){
-		s[i] = c;
+	for (i = 0; i < MAXLINE -1 && (c = getchar) != EOF && c != '\n'; ++i)
+		line[i] = c;
+	if (c == '\n') {
+		line[i] = c;
 		++i;
 	}
-	s[i] = '\0';
+	line[i] = '\0';
 	return i;
 }
 
-void copy(char to[], char from[])
+void copy(void)
 {
 	int i;
+	extern char line[], longest[];
 
 	i = 0;
-	while ((to[i] = from[i]) != '\0')
+	while ((longest[i] = line[i]) != '\0')
 		++i;
 }
+
